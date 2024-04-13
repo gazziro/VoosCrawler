1d1548f001043f7d079f4b6f20018e19f2848e83
Gabriel <gabgazziro@gmail.com>
2024-04-12 21:50:21 -0300

    primeiro commit refatorando o código

--- voos.py
+++ voos.py
@@ -3,66 +3,70 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-chrome = Service('C:\Program Files (x86)\chromedriver.exe') #arquivo executável que abre o chrome 
-driver = webdriver.Chrome(service=chrome)
-url = "https://www.decolar.com/passagens-aereas/SAO/ORL?from=SB&di=1-0"
-driver.get(url)
+def get_info(information):
+    return [i.text for i in information]
 
-precos = []
-empresas = []
-datas = []
-horarios = []
-horario_chegada =[]
-horario_saida = []
-data_ida = []
-data_saida = []
-stopwords = ['1 parada', '2 paradas', 'Direto'] #serve para tirar essas palavras da busca em "horarios"
-contador = 0 #serve para organizar, se utilizasse o list.index(), as datas com o mesmo número, se repetiriam e causariam uma confusão
-cont = 0 #serve para organizar, se utilizasse o list.index(), as datas com o mesmo número, se repetiriam e causariam uma confusão
-falta_empresa = "2 empresas" #um problema é que às vezes tem duas empresas para uma passagem, 
-#dessa forma, não fica o nome de nenhuma empresa, apenas o logo delas
+def get_schedule(schedule):
+    # serve para tirar essas palavras da busca em "horarios"
+    stopwords = ['1 parada', '2 paradas', 'Direto'] 
+    
+    schedule = [i for i in schedule if i.text !='- ']
+    schedule_clean = [i.text for i in schedule if i.text not in stopwords]
+    return schedule_clean
 
-preco_voo = driver.find_elements(By.XPATH, '//span[@Class="pricebox-big-text price"]')
-empresa = driver.find_elements(By.XPATH,"//span[@Class='name']")
-data = driver.find_elements(By.XPATH,"//div[@Class='date -eva-3-bold route-info-item-date lowercase']")
-horario = driver.find_elements(By.XPATH,"//span[@Class='stops-text text -eva-3-tc-gray-2']")
+def get_fly(driver):
+    price = driver.find_elements(
+        By.XPATH, '//span[@Class="pricebox-big-text price"]'
+    )
+    company = driver.find_elements(
+        By.XPATH,"//span[@Class='name']"
+    )
+    date = driver.find_elements(
+        By.XPATH,"//div[@Class='date -eva-3-bold route-info-item-date lowercase']"
+    )
+    schedule = driver.find_elements(
+        By.XPATH,"//span[@Class='stops-text text -eva-3-tc-gray-2']"
+    )
+    
+    price_listed = get_info(price)
+    company_listed = get_info(company)
+    date_listed = get_info(date)
+    schedule_listed = get_schedule(schedule)
+
+    return price_listed, company_listed, date_listed, schedule_listed
+
+def resto(price_listed, company_listed, date_listed, schedule_listed):
+    # contador = 0 serve para organizar, se utilizasse o list.index(), 
+    # as datas com o mesmo número, se repetiriam e causariam uma confusão
+    # cont = 0 #serve para organizar, se utilizasse o list.index(), as datas 
+    # com o mesmo número, se repetiriam e causariam uma confusão
+    
+    horario_chegada = [x for i, x in enumerate(schedule_listed) if i%2==0]
+    horario_saida = [x for i, x in enumerate(schedule_listed) if i%2==1]
+
+    data_ida = [x for i, x in enumerate(date_listed) if i%2==0]
+    data_saida = [x for i, x in enumerate(date_listed) if i%2==1]
+
+    while len(company_listed) < len(price_listed): 
+    #um problema é que às vezes tem duas company_listed para uma passagem, 
+    #dessa forma, não fica o nome de nenhuma empresa no site, apenas o logo delas.
+        company_listed.append("2 empresas")
+
+    data = pd.DataFrame({
+        'Preço':price_listed, 
+        'Empresa':company_listed, 
+        'Data Ida':data_ida,
+        'Data Volta':data_saida, 
+        'Horario Ida':horario_chegada, 
+        'Horario Volta':horario_saida,
+    })
+    data.to_excel('voo.xlsx', sheet_name='sheet1', index=False)
 
 
-for i in preco_voo:
-    precos.append(i.text)
-
-for i in empresa:
-    empresas.append(i.text)
-
-for i in data:
-    datas.append(i.text)
-
-for i in horario:
-    horarios.append(i.text)
-    if i.text == '- ':
-        del horarios[-1]
-horarios = [i for i in horarios if i not in stopwords] #eliminando os elementos indesejáveis
-
-for i in horarios:
-    if cont%2==0 or cont == 0: #utilizando o cont para organizar os horarios
-        horario_chegada.append(i) 
-        cont = cont +1 
-    else:
-        horario_saida.append(i) 
-        cont = cont +1
-
-for i in datas:
-    if contador%2==0 or contador == 0: #utilizando o contador para organizar as datas
-        data_ida.append(i)
-        contador = contador +1 
-    else:
-        data_saida.append(i)
-        contador = contador +1 
-
-while len(empresas) < len(precos): #um problema é que às vezes tem duas empresas para uma passagem, 
-#dessa forma, não fica o nome de nenhuma empresa no site, apenas o logo delas.
-    empresas.append(falta_empresa)
-
-data = pd.DataFrame({'Preço':precos, 'Empresa':empresas, 'Data Ida':data_ida,'Data Volta':data_saida, 
-'Horario Ida':horario_chegada, 'Horario Volta':horario_saida,}) #pandas básico para criar uma excel
-data.to_excel('voo.xlsx', sheet_name='sheet1')
+if __name__ == "__main__":
+    url = "https://www.decolar.com/passagens-aereas/SAO/ORL?from=SB&di=1-0"
+    driver = webdriver.Chrome()
+    driver.get(url)
+    price_listed, company_listed, date_listed, schedule_listed = get_fly(driver=driver)
+    resto(price_listed, company_listed, date_listed, schedule_listed)
+    print("oi")
